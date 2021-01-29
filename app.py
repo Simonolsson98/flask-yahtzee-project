b3d7600ae56300040e9f5ff62c9228a02ccf564d
Simonolsson98 <Simol@student.chalmers.se>
2021-01-29 23:25:22 +0100

    fixing the math a bit

--- app.py
+++ app.py
@@ -17,23 +17,23 @@ def after_input():
         die3 = list_of_dices[2]
         list_of_probs = calc_prob(die1, die2, die3, throws)
 
-        return render_template("index.html", three_of_a_kind = f"{list_of_probs[0]*100}%", four_of_a_kind = f"{list_of_probs[1]*100}%", 
-        full_house_prob = f"{list_of_probs[2]*100}%", yahtzee_prob = f"{list_of_probs[3]*100}%", old_1 = die1, old_2 = die2, old_3 = die3)
+        return render_template("index.html", three_of_a_kind = f"{list_of_probs[0]}%", four_of_a_kind = f"{list_of_probs[1]}%", 
+        full_house_prob = f"{list_of_probs[2]}%", yahtzee_prob = f"{list_of_probs[3]}%", old_1 = die1, old_2 = die2, old_3 = die3)
     else:
         return "please return your dice values"
 
 def calc_prob(die1, die2, die3, throwcount):
     if throwcount == "1":
         if(die1 == die2 and die2 == die3):
-            return [1, round(2/6, 2), round(1/6, 2), round(1/(6**2), 2)]
+            return [1 * 100, round(2/6 * 100, 2), round(1/6 * 100, 2), round(1/(6**2) * 100, 2)]
         elif(die1 == die2 or die2 == die3 or die1 == die3):
-            return [round(3/6, 2), round(347/4997, 2), round(0.2222, 2), round(1/(6**3), 4)]
+            return [round(3/6 * 100, 2), round((347/4997) * 100, 2), round(0.2222 * 100, 2), round(1/(6**3) * 100, 4)]
         return [-1,-1,-1,-1]
     else: #throwcount = 2
         if(die1 == die2 and die2 == die3):
-            return [1, round(4/6, 2), round(2/6, 2), round(2/(6**2), 2)]
+            return [1 * 100, round(4/6 * 100, 2), round(2/6 * 100, 2), round(2/(6**2) * 100, 2)]
         elif(die1 == die2 or die2 == die3 or die1 == die3):
-            return [round(132/216, 2), round(0.11574*2, 2), round(0.4444, 2), round(1/(6**3), 4)]
+            return [round((132/216) * 100, 2), round(0.11574*2 * 100, 2), round(0.4444 * 100, 2), round(1/(6**3) * 100, 4)]
         return [-1,-1,-1,-1]
 
 
